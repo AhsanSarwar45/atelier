5b6dc1aacc325db5461c4073b407e87a57d3560c
AhsanSarwar45 <ahsansarwar.as45@gmail.com>
2026-09-11 18:19:39 +0500

    chore(packaging): release 0.22.3

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
 
           frontend = pkgs.buildNpmPackage {
             pname = "atelier-frontend";
-            version = "0.22.2";
+            version = "0.22.3";
             src = ./.;
 
             nodejs = pkgs.nodejs_22;
@@ -41,7 +41,7 @@
         {
           default = pkgs.rustPlatform.buildRustPackage {
             pname = "atelier";
-            version = "0.22.2";
+            version = "0.22.3";
             src = ./.;
 
             cargoLock.lockFile = ./server/Cargo.lock;
